81ecc2ac3b9705fa6f7ff5f9a943203d7d239c13
pkmitl205 <58030205@kmitl.ac.th>
2017-10-25 03:00:50 +0700

    Add "PostQuitMessage(0);" to WM_DESTROY

--- Easy/Easy/Easy.cpp
+++ Easy/Easy/Easy.cpp
@@ -24,7 +24,7 @@ LRESULT CALLBACK WindowsProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 
 	case WM_DESTROY:
 	{
-
+		PostQuitMessage(0);
 	}break;
 	}
 	return(DefWindowProc(hwnd, msg, wparam, lparam));
