b07a0a15c37a2e76f9e484ef7a0a83d49f7c1d19
pkmitl205 <58030205@kmitl.ac.th>
2017-10-24 23:50:41 +0700

    การสร้าง Icon

--- Easy/Easy/Easy.cpp
+++ Easy/Easy/Easy.cpp
@@ -2,9 +2,71 @@
 ============================================================================
 Name        : Easy
 Author      : Pakon Ruchirekserikun
-Version     : 
+Version     :
 Copyright   : Your copyright notice
 Description : Easy.cpp
 ============================================================================
 */
 
+#include <windows.h>
+#include <windowsx.h>
+
+#include  "resource.h"
+
+LRESULT CALLBACK WindowsProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	switch (msg)
+	{
+	case WM_PAINT:
+	{
+
+	}break;
+
+	case WM_DESTROY:
+	{
+
+	}break;
+	}
+	return(DefWindowProc(hwnd, msg, wparam, lparam));
+}
+
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hprevinstance, LPSTR lpcmdline, int ncmdshow)
+{
+	WNDCLASSEX winclass;
+	winclass.cbSize = sizeof(WNDCLASSEX);
+	winclass.style = CS_VREDRAW | CS_HREDRAW | CS_OWNDC | CS_DBLCLKS;
+	winclass.lpfnWndProc = WindowsProc;
+	winclass.cbClsExtra = 0;
+	winclass.cbWndExtra = 0;
+	winclass.hInstance = hinstance;
+
+	winclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	winclass.lpszClassName = "MyWin";
+	winclass.lpszMenuName = NULL;
+	winclass.hIcon = LoadIcon(hinstance, MAKEINTRESOURCE(IDI_ICON1));
+	winclass.hCursor = LoadCursor(hinstance, IDC_ARROW);
+	winclass.hIconSm = LoadIcon(hinstance, MAKEINTRESOURCE(IDI_ICON1));
+
+	RegisterClassEx(&winclass);
+
+	HWND hwnd;
+	MSG msg;
+
+	hwnd = CreateWindowEx(NULL, "MyWin", "Win32 Window", WS_OVERLAPPEDWINDOW, 100, 100,
+		400, 400, NULL, NULL, hinstance, NULL);
+
+	ShowWindow(hwnd, ncmdshow);
+	UpdateWindow(hwnd);
+
+	while (1)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE))
+		{
+			if (!GetMessage(&msg, NULL, 0, 0))
+				return msg.wParam;
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+	return(msg.wParam);
+}
