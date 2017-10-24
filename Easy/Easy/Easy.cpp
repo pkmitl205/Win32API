6e5a1dab3532c723994d03fbcd8d3324ca5eb6e1
pkmitl205 <58030205@kmitl.ac.th>
2017-10-24 21:55:50 +0700

    My FirstApplication

--- Easy/Easy/Easy.cpp
+++ Easy/Easy/Easy.cpp
@@ -8,3 +8,63 @@ Description : Easy.cpp
 ============================================================================
 */
 
+#include <windows.h>
+#include <windowsx.h>
+
+LRESULT CALLBACK WindowsProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	switch (msg)
+	{
+		case WM_PAINT :
+		{
+
+		}break;
+
+		case WM_DESTROY :
+		{
+
+		}break;
+	}
+	return(DefWindowProc(hwnd, msg, wparam, lparam));
+}
+
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hprevinstance, LPSTR lpcmdline, int ncmdshow)
+{
+	WNDCLASSEX winclass;
+	winclass.cbSize = sizeof(WNDCLASSEX);
+	winclass.style = CS_VREDRAW | CS_HREDRAW | CS_OWNDC | CS_DBLCLKS;
+	winclass.lpfnWndProc = WindowsProc;
+	winclass.cbClsExtra = 0;
+	winclass.cbWndExtra = 0;
+	winclass.hInstance = hinstance;
+
+	winclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	winclass.lpszClassName = "MyWin";
+	winclass.lpszMenuName = NULL;
+	winclass.hIcon = LoadIcon(hinstance, IDI_APPLICATION);
+	winclass.hCursor = LoadCursor(hinstance, IDC_ARROW);
+	winclass.hIconSm = LoadIcon(hinstance, IDI_APPLICATION);
+
+	RegisterClassEx(&winclass);
+
+	HWND hwnd;
+	MSG msg;
+
+	hwnd = CreateWindowEx(NULL, "MyWin", "Win32 Window", WS_OVERLAPPEDWINDOW, 100, 100,
+		400, 400, NULL, NULL, hinstance, NULL);
+
+	ShowWindow(hwnd, ncmdshow);
+	UpdateWindow(hwnd);
+
+	while (1)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE))
+		{
+			if (!GetMessage(&msg, NULL, 0, 0))
+				return msg.wParam;
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+	return(msg.wParam);
+}
